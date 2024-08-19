73e09b7873ee063fd05f678162258a71bdc6dd6d
nrykavanade <n.rykaczewski@avanade.com>
2024-08-19 13:34:03 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+  return {
+    skills: ['JavaScript', 'React', 'Node', 'Express', 'MongoDB', 'HTML', 'CSS'],
+    bio: 'I love to code and learn new things!',
+    experience: 'I have been coding for over 5 years and love to help others learn how to code.'
+  }
+}
